2cab5b6d8a8a7160e39483202d9a922d164fc972
byeonheejae <pion0458@gmail.com>
2025-06-24 10:49:59 +0900

    feat: Follow Service Interface

--- src/main/java/com/example/ootd/domain/follow/service/FollowService.java
+++ src/main/java/com/example/ootd/domain/follow/service/FollowService.java
@@ -1,5 +1,18 @@
 package com.example.ootd.domain.follow.service;
 
+import com.example.ootd.domain.follow.dto.FollowCreateRequest;
+import com.example.ootd.domain.follow.dto.FollowDto;
+import com.example.ootd.domain.follow.dto.FollowSummaryDto;
+import java.util.UUID;
+
 public interface FollowService {
 
+    /**
+     * @param request follower and followee IDs
+     */
+  FollowDto createFollow(FollowCreateRequest request);
+
+  FollowSummaryDto getSummaryFollow(UUID userId);
+
+  void deleteFollow(UUID followId);
 }

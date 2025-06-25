a630815ad86235c18703f5003ce662d859ea7aa6
Kiki1875b <kevinheo0413@gmail.com>
2025-06-25 17:36:27 +0900

    feat: jwt filter, service 구현 및 세션 객체 추가

--- src/main/java/com/example/ootd/config/SecurityConfig.java
+++ src/main/java/com/example/ootd/config/SecurityConfig.java
@@ -1,25 +1,66 @@
 package com.example.ootd.config;
 
+import com.example.ootd.security.CustomUserDetailsService;
+import com.example.ootd.security.CustomUsernamePasswordAuthenticationFilter;
+import com.example.ootd.security.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
   /**
    * 기초 세팅을 위한 임시 필터체인
    */
   @Bean
-  public SecurityFilterChain chain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain chain(HttpSecurity http,
+      AuthenticationManager manager,
+      CustomUsernamePasswordAuthenticationFilter customFilter) throws Exception {
+    customFilter.setAuthenticationManager(manager);
+
     http
         .csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        .formLogin(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        .addFilterAt(customFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
+
+  @Bean
+  public AuthenticationManager authManager(AuthenticationConfiguration configuration)
+      throws Exception {
+    return configuration.getAuthenticationManager();
+  }
+
+  @Bean
+  public SessionRegistry sessionRegistry(){
+    return new SessionRegistryImpl();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
 }
---
+++ src/main/java/com/example/ootd/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.example.ootd.security.jwt;
+
+import com.example.ootd.security.CustomUserDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
+
+  // 토큰을 검증하고 SecurityContext 에 현제 인증정보 저장
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    String token = request.getHeader("Authorization");
+    if(token != null && token.startsWith("Bearer")){
+      try {
+        token = token.substring(7);
+        if(!jwtService.validateToken(token)){
+          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+          return;
+        }
+
+        String email = jwtService.extractEmail(token);
+        CustomUserDetails userDetails = (CustomUserDetails) userDetailsService.loadUserByUsername(email);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      } catch (Exception e){
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
+}

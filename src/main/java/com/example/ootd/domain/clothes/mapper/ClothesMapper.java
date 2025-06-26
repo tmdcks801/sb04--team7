324b5b1aa22db0b7cf25eb1df26605eb1bbf4d16
hyanyul <tjdwl0808@gmail.com>
2025-06-26 17:55:00 +0900

    feat: 의상 속성 정의 CRUD 기능 추가

--- src/main/java/com/example/ootd/domain/clothes/mapper/ClothesMapper.java
+++ src/main/java/com/example/ootd/domain/clothes/mapper/ClothesMapper.java
@@ -5,11 +5,11 @@ import com.example.ootd.domain.clothes.entity.Clothes;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-//// TODO: ClothesDto의 attribute도 변환 필요 -> 해당 기능 구현 시 수정
-//@Mapper(componentModel = "spring")
-//public interface ClothesMapper {
-//
-//  @Mapping(source = "user.id", target = "ownerId")
-//  @Mapping(source = "image.url", target = "imageUrl")
-//  ClothesDto toDto(Clothes clothes);
-//}
+// TODO: ClothesDto의 attribute도 변환 필요 -> 해당 기능 구현 시 수정
+@Mapper(componentModel = "spring")
+public interface ClothesMapper {
+
+  @Mapping(source = "user.id", target = "ownerId")
+  @Mapping(source = "image.url", target = "imageUrl")
+  ClothesDto toDto(Clothes clothes);
+}

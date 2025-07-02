361437704ec73397d0f937b9065266e18a249fc9
hyanyul <tjdwl0808@gmail.com>
2025-07-02 18:24:15 +0900

    feat: feed 관련 예외처리 추가

---
+++ src/main/java/com/example/ootd/exception/feed/FeedException.java
@@ -0,0 +1,15 @@
+package com.example.ootd.exception.feed;
+
+import com.example.ootd.exception.ErrorCode;
+import com.example.ootd.exception.OotdException;
+
+public class FeedException extends OotdException {
+
+  public FeedException(ErrorCode errorCode) {
+    super(errorCode);
+  }
+
+  public FeedException(ErrorCode errorCode, Throwable cause) {
+    super(errorCode, cause);
+  }
+}

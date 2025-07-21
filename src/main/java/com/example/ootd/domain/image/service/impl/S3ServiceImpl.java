feed6cfd8e9ae8969155962971c994cc62f332b5
de31 <sungsam9709@gmail.com>
2025-07-21 09:36:06 +0900

    fix : 테스트 환경에서 caffeine으로 적용하기

--- src/main/java/com/example/ootd/domain/image/service/impl/S3ServiceImpl.java
+++ src/main/java/com/example/ootd/domain/image/service/impl/S3ServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.ootd.domain.image.service.impl;
 import com.example.ootd.domain.image.entity.Image;
 import com.example.ootd.domain.image.service.S3Service;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Slf4j
@@ -69,6 +71,26 @@ public class S3ServiceImpl implements S3Service {
     } catch (Exception e) {
       log.error("S3 삭제 실패", e);
       throw new RuntimeException("S3 파일 삭제 실패", e);
+    }
+  }
+
+  @Override
+  public InputStream getFileInputStream(String fileName) {
+    try {
+      log.info("Attempting to read file from S3: bucket={}, key={}", bucket, fileName);
+      
+      GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+          .bucket(bucket)
+          .key(fileName)
+          .build();
+
+      InputStream inputStream = s3Client.getObject(getObjectRequest);
+      log.info("Successfully retrieved file from S3: {}", fileName);
+      return inputStream;
+
+    } catch (Exception e) {
+      log.error("S3 파일 읽기 실패: bucket={}, key={}, error={}", bucket, fileName, e.getMessage(), e);
+      throw new RuntimeException("S3 파일 읽기 실패: " + fileName, e);
     }
   }
 }

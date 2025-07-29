32303fa55c40c87f423b0a461105eec157e4e13e
hyanyul <tjdwl0808@gmail.com>
2025-07-29 14:03:27 +0900

    fix: 프로필 사진 수정 안되는 문제 해결

--- src/main/java/com/example/ootd/domain/user/controller/UserController.java
+++ src/main/java/com/example/ootd/domain/user/controller/UserController.java
@@ -56,7 +56,7 @@ public class UserController {
       produces = MediaType.APPLICATION_JSON_VALUE
   )
   public ResponseEntity<ProfileDto> updateUserProfile(@PathVariable UUID userId, @RequestPart
-      ProfileUpdateRequest request, @RequestPart(required = false) MultipartFile profile){
+      ProfileUpdateRequest request, @RequestPart(required = false, value = "image") MultipartFile profile){
     return ResponseEntity.ok(userService.updateUserProfile(userId, request, profile));
   }
 
